321a20a6bff6e234345420b58dd81102099809d0
Shaddatic <Shaddatic@gmail.com>
2024-07-09 16:29:19 +0100

    Added initial API files

---
+++ sa2b-input-controls/ic_api.c
@@ -0,0 +1,3 @@
+#include <sa2b/core.h>
+
+#include <ic_api/icapi_internal.h>
---
+++ sa2b-input-controls/ic_api/icapi_internal.h
@@ -0,0 +1,24 @@
+/*
+*   SA2 Input Controls - '/ic_api/icapi_internal.h'
+*
+*   Description:
+*     Internal header for the Input Controls mod API
+*
+*   Contributors:
+*     - Shaddatic
+*/
+#ifndef H_IC_API_INTERNAL
+#define H_IC_API_INTERNAL
+
+/************************/
+/*  External Headers    */
+/************************/
+/****** External API ****************************************************************/
+#include <../api/inputcont_api.h>
+
+/************************/
+/*  API Structs         */
+/************************/
+
+
+#endif/*H_IC_API_INTERNAL*/
