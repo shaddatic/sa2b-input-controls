275e520f3a673c55d2bb8e3f3cc211998a840963
Shaddatic <Shaddatic@gmail.com>
2024-07-18 20:20:49 +0100

    Add SDL limits

--- sa2b-input-controls/ic_limits.h
+++ sa2b-input-controls/ic_limits.h
@@ -15,20 +15,29 @@
 /************************/
 /****** PDS Pad *********************************************************************/
 /****** Analog Stick ********************************************************/
-#define PDSLIM_XY_MIN   (-128)  /* 100% up/left                             */
-#define PDSLIM_XY_MAX   (+128)  /* 100% down/right/down                     */
+#define PDSLIM_XY_MIN   (-128) /* 100% up/left                              */
+#define PDSLIM_XY_MAX   (+128) /* 100% down/right                           */
 
 /****** Analog Trigger ******************************************************/
-#define PDSLIM_LR_MIN   (+  0)  /* at rest                                  */
-#define PDSLIM_LR_MAX   (+255)  /* 100% down                                */
+#define PDSLIM_LR_MIN   (+  0) /* at rest                                  */
+#define PDSLIM_LR_MAX   (+255) /* 100% down                                */
+
+/****** Raw Gamepad *****************************************************************/
+/****** Analog Stick ********************************************************/
+#define GPDLIM_XY_MIN   (-32768) /* 100% up/left                            */
+#define GPDLIM_XY_MAX   (+32767) /* 100% down/right                         */
+
+/****** Analog Trigger ******************************************************/
+#define GPDLIM_LR_MIN   (+    0) /* at rest                                 */
+#define GPDLIM_LR_MAX   (+32767) /* 100% down                               */
 
 /****** User Input ******************************************************************/
 /****** Analog Stick ********************************************************/
-#define USRLIM_XY_MIN   (-1.f)  /* 100% up/left                             */
-#define USRLIM_XY_MAX   (+1.f)  /* 100% down/right/down                     */
+#define USRLIM_XY_MIN   (-1.f) /* 100% up/left                              */
+#define USRLIM_XY_MAX   (+1.f) /* 100% down/right                           */
 
 /****** Analog Trigger ******************************************************/
-#define USRLIM_LR_MIN   (+0.f)  /* at rest                                  */
-#define USRLIM_LR_MAX   (+1.f)  /* 100% down                                */
+#define USRLIM_LR_MIN   (+0.f) /* at rest                                   */
+#define USRLIM_LR_MAX   (+1.f) /* 100% down                                 */
 
 #endif/*H_IC_LIMITS*/
