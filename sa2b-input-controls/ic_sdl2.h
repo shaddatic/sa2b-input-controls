74636af54839b7621df9d0d265844ebf627e0c08
Shaddatic <shaddatic@proton.me>
2024-07-24 12:48:30 +0100

    Added function descriptions

--- sa2b-input-controls/ic_sdl2.h
+++ sa2b-input-controls/ic_sdl2.h
@@ -16,11 +16,36 @@ EXTERN_START
 /*  Prototypes          */
 /************************/
 /****** Module Init *****************************************************************/
+/*
+*   Description:
+*     Init SDL module
+*
+*   Returns:
+*     If Init was successful
+*/
 bool    ICSDL_Init( void );
+/*
+*   Description:
+*     SDL Exit function for OnExit
+*/
 void    ICSDL_Exit( void );
 
 /****** Input Controls API **********************************************************/
+/*
+*   Description:
+*     Get DLL handle for mounted SDL library
+*
+*   Returns:
+*     The 'HMODULE' or 'dll_handle' to the SDL DLL library
+*/
 void*   ICSDL_GetHandle( void );
+/*
+*   Description:
+*     Get DLL handle for mounted SDL library
+*
+*   Returns:
+*     A pointer to the DLL export, or nullptr if no export is found
+*/
 void*   ICSDL_GetExport( const char* cExName );
 
 EXTERN_END
