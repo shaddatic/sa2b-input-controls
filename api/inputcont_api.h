a7acf387e20f9b27d8d1716dfa2c65af8db7bbb9
Shaddatic <shaddatic@proton.me>
2025-05-14 18:04:08 +0100

    Remove old `utf8` definition for UTF-8 strings

--- api/inputcont_api.h
+++ api/inputcont_api.h
@@ -21,7 +21,7 @@ typedef float               f32;    /* 4 byte real number                       
 typedef double              f64;    /* 8 byte real number                           */
 
 /****** String Formats **************************************************************/
-typedef char                utf8;   /* supports UTF-8 strings                       */
+typedef char                c8;     /* supports UTF-8 strings                       */
 
 /************************/
 /*  Opaque Types        */
@@ -344,10 +344,10 @@ typedef struct
     *   Returns:
     *     The value of the config entry, or 'def' if no entry exists
     */
-    int32_t    (__cdecl* GetInt)(    const utf8* uSect, const utf8* uKey, int32_t     def );
-    bool       (__cdecl* GetBool)(   const utf8* uSect, const utf8* uKey, bool        def );
-    f64        (__cdecl* GetFloat)(  const utf8* uSect, const utf8* uKey, f64         def );
-    const utf8*(__cdecl* GetString)( const utf8* uSect, const utf8* uKey, const utf8* def );
+    int32_t  (__cdecl* GetInt)(    const c8* uSect, const c8* uKey, int32_t   def );
+    bool     (__cdecl* GetBool)(   const c8* uSect, const c8* uKey, bool      def );
+    f64      (__cdecl* GetFloat)(  const c8* uSect, const c8* uKey, f64       def );
+    const c8*(__cdecl* GetString)( const c8* uSect, const c8* uKey, const c8* def );
 }
 ICAPI_CONFIG;
 
