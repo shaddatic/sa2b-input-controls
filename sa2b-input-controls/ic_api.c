2d573c1f09962bb3ce6b6cfb022b1bdf24d03ecc
Shaddatic <shaddatic@proton.me>
2025-05-14 20:21:09 +0100

    Make core api structure constant, to match same change in Render Fix

--- sa2b-input-controls/ic_api.c
+++ sa2b-input-controls/ic_api.c
@@ -33,11 +33,20 @@ typedef void(__cdecl IC_INIT)(const ICAPI_CORE*, const char*, const HelperFuncti
 /************************/
 /****** API Struct ******************************************************************/
 EXPORT_DLL
-ICAPI_CORE icapi_core =
+const ICAPI_CORE icapi_core =
 {
     .version = ICAPI_CORE_VER,
 
     .ic_version = { 1,1,0,0 },
+
+    .pApiFeature  = &icapi_feature,
+    .pApiConfig   = &icapi_config,
+    .pApiUser     = &icapi_user,
+    .pApiGamepad  = &icapi_gamepad,
+    .pApiKeyboard = &icapi_keyboard,
+    .pApiMouse    = &icapi_mouse,
+    .pApiWindow   = &icapi_window,
+    .pApiSdl      = &icapi_sdl,
 };
 
 /************************/
@@ -64,23 +73,11 @@ ApiCallByFuncName(const char* const cExName)
 void
 ICAPI_Init(void)
 {
-    icapi_core.pApiConfig = &icapi_config,
-    icapi_core.pApiSdl    = &icapi_sdl;
-
     ApiCallByFuncName("IC_EarlyInit");
 }
 
 void
 ICAPI_End(void)
 {
-    icapi_core.pApiFeature = &icapi_feature;
-    icapi_core.pApiUser     = &icapi_user;
-    icapi_core.pApiGamepad  = &icapi_gamepad;
-    icapi_core.pApiKeyboard = &icapi_keyboard;
-    icapi_core.pApiMouse    = &icapi_mouse;
-    icapi_core.pApiWindow   = &icapi_window;
-
     ApiCallByFuncName("IC_Init");
-
-    icapi_core.pApiConfig = nullptr;
 }
